308a6852e981763ee82a401b77800321c6d2fca6
Abhilash-Perficient <abhilash.satpute@perficient.com>
2020-01-31 11:31:46 +0530

    Add project files.

---
+++ Learn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Learn.Startup))]
+namespace Learn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
